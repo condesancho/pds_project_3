23e728c49341b446a19eacd5e866dcddec17ac8d
Gwyn <harry.filis@yahoo.gr>
2021-01-25 22:25:36 +0200

    test file - main

--- src/test.c
+++ src/test.c
@@ -8,19 +8,25 @@ int main(void){
 
     int patch_size = 5;
     int row = 0;
-    int col = 3;
+    int col = 0;
 
-    float *patch = patch_finder(X, patch_size, row, col, size, size);
+    float *pad = pad_array(X,5,5,5);
 
     printf("\n");
+    // for (int i=0; i<size+patch_size-1; i++){
+    //     for (int j=0; j<size+patch_size-1; j++){
+    //         printf("%f ", pad[i*(size+patch_size-1) + j]);
+    //     }
+    //     printf("\n");
+    // }
+    float *patch = patch_finder(pad,patch_size,row,col,size,size);
     for (int i=0; i<patch_size; i++){
         for (int j=0; j<patch_size; j++){
             printf("%f ", patch[i*patch_size + j]);
         }
         printf("\n");
     }
-
+    free(pad);
     free(patch);
-
     return 0;
 }
